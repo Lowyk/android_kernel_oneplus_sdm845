4d27d70fdffd3375e33a084cb3c1605e8cd7ab6e
Lowyk <149260287+Lowyk@users.noreply.github.com>
2026-09-20 02:42:15 +0200

    mm: backport MADV_WIPEONFORK/MADV_KEEPONFORK from mainline d2cd9ede6e19

--- kernel/fork.c
+++ kernel/fork.c
@@ -693,7 +693,10 @@ static __latent_entropy int dup_mmap(struct mm_struct *mm,
 		rb_parent = &tmp->vm_rb;
 
 		mm->map_count++;
-		retval = copy_page_range(mm, oldmm, mpnt);
+		if (!(tmp->vm_flags & VM_WIPEONFORK))
+			retval = copy_page_range(mm, oldmm, mpnt);
+		else
+			retval = 0;
 
 		if (tmp->vm_ops && tmp->vm_ops->open)
 			tmp->vm_ops->open(tmp);
